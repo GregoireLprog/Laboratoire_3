f3b859bcbf22c70ae5741b23584ad7f116b41360
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-02-07 14:24:30 -0500

    mod

--- modifications.py
+++ modifications.py
@@ -1,1 +1,4 @@
-""" ajouter une condtions si les nombres sont égaux """
+""" ajouter une condtions si les trois nombres son égaux
+si les 3 sont égaux = le dit a l'utilisateur
+ensuite offre un choix, soit de jouer a roche papier ciseaux ou de fermer le programme
+si l'utilisateur choisi de jouer, une partie de roche papier ciseaux commence"""
--- prepLab.py
+++ prepLab.py
@@ -22,49 +22,29 @@ print(result)
 input4 = str(input("Voulez vous jouer a roche papier sciscaux? (oui/non) : "))
 if input4 == "non":
     print("Au revoir!")
+    exit()
 elif input4 == "oui":
     print("Choisissez entre roche, papier ou ciseaux.")
 else:
-    
+    import random
 
-import random
+    options = ["Rock", "Paper", "Scissors"]
 
- 
+    user_choice = input("Choose Rock, Paper, or Scissors: ")
+    computer_choice = random.choice(options)
 
-options = ["Rock", "Paper", "Scissors"]
+    print("You chose: ", user_choice)
+    print("Computer chose: ", computer_choice)
 
- 
-
-user_choice = input("Choose Rock, Paper, or Scissors: ")
-
-computer_choice = random.choice(options)
-
- 
-
-print("You chose: ", user_choice)
-
-print("Computer chose: ", computer_choice)
-
- 
-
-if user_choice == computer_choice:
-
-    print("It's a tie!")
-
-elif user_choice == "Rock" and computer_choice == "Scissors":
-
-    print("You win!")
-
-elif user_choice == "Paper" and computer_choice == "Rock":
-
-    print("You win!")
-
-elif user_choice == "Scissors" and computer_choice == "Paper":
-
-    print("You win!")
-
-else:
-
-    print("Computer wins!")
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif user_choice == "Rock" and computer_choice == "Scissors":
+        print("You win!")
+    elif user_choice == "Paper" and computer_choice == "Rock":
+        print("You win!")
+    elif user_choice == "Scissors" and computer_choice == "Paper":
+        print("You win!")
+    else:
+        print("Computer wins!")
 
 # Imprimer le résultat
