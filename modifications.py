0f8686c167b663cedac5f8e9dd391c06efb55744
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-02-07 14:43:36 -0500

    mo

--- modifications.py
+++ modifications.py
@@ -1,4 +1,5 @@
 """ ajouter une condtions si les trois nombres son égaux
 si les 3 sont égaux = le dit a l'utilisateur
 ensuite offre un choix, soit de jouer a roche papier ciseaux ou de fermer le programme
-si l'utilisateur choisi de jouer, une partie de roche papier ciseaux commence"""
+si l'utilisateur choisi de jouer, une partie de roche papier ciseaux commence
+on rajoute le code pour un jeu de roche papier ciseaux contre l'ordinateur"""
--- prepLab.py
+++ prepLab.py
@@ -16,35 +16,34 @@ elif input2 > input1 and input2 > input3:
     result = f"Le deuxième nombre {input2} est le plus grand."
 else:
     result = f"Le troisième nombre {input3} est le plus grand."
-if input1 == input2 and input2 == input3:
+if input1 == input2 and input2 == input3: # si les 3 nombres sont égaux
     result = "Les trois nombres sont égaux, prépare toi."
 print(result)
 input4 = str(input("Voulez vous jouer a roche papier sciscaux? (oui/non) : "))
 if input4 == "non":
     print("Au revoir!")
-    exit()
-elif input4 == "oui":
-    print("Choisissez entre roche, papier ou ciseaux.")
+    exit() #ferme le programme si "non" est entré
+elif input4 == "oui": # si "oui" est entré, le programme continue
+    print("Choisissez entre roche, papier ou ciseaux.") # roche papier ciseaux classique, (une partie)
 else:
-    import random
+    import random # dit a l'ordi de choisir une valeur au hasard q'on vas définir
 
-    options = ["Rock", "Paper", "Scissors"]
+    options = ["roche", "papier", "ciseaux"] # répertoire des choix possibles de l'ordinateur et utilisateur
 
-    user_choice = input("Choose Rock, Paper, or Scissors: ")
+    user_choice = input("Choose roche, papier, or ciseaux:") # demande a l'utilisateur de choisir
     computer_choice = random.choice(options)
 
-    print("You chose: ", user_choice)
-    print("Computer chose: ", computer_choice)
-
+    print("You chose: ", user_choice) # affiche ce que l'utilisateur a choisi
+    print("Computer chose: ", computer_choice) # affiche ce que l'ordinateur a choisi
+# condition pour déterminer le gagnant
     if user_choice == computer_choice:
         print("It's a tie!")
-    elif user_choice == "Rock" and computer_choice == "Scissors":
+    elif user_choice == "roche" and computer_choice == "ciseaux":
         print("You win!")
-    elif user_choice == "Paper" and computer_choice == "Rock":
+    elif user_choice == "papier" and computer_choice == "roche":
         print("You win!")
-    elif user_choice == "Scissors" and computer_choice == "Paper":
+    elif user_choice == "ciseaux" and computer_choice == "papier":
         print("You win!")
     else:
         print("Computer wins!")
-
-# Imprimer le résultat
+# fin du programme
