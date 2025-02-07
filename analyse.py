cc24b1bf3371fb391523249f47f7725b736c878a
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-02-07 14:18:50 -0500

    Create analyse.py

---
+++ analyse.py
@@ -0,0 +1,11 @@
+""" dans les coditions, il faudrait en rajouter pour pouvoir être en mesure décrire des chiffres égaux.
+    Il faudrait aussi ajouter des conditions pour vérifier si les entrées sont des nombres.
+    on peut aussi rajouter des messages d'erreurs pour les entrées qui ne sont pas des nombres.
+    et de plus, ajouter un message pour dire que les nombres sont égaux.
+    Ainsi, demander à l'utilisateur de jouer a roche papier sciscaux ou de fermer le programme.
+    
+    pour simplifier, on pourrait demander a l'ordinateur quel nombre est le plus grand entre les trois et afficher le plus grand
+    
+    les normes de codage son respectées, les espace et virgule sont au bon endroit.
+    
+    l'éexpérience utilisateur est simplement consittué de choix qu'il pourrait faire pour continuer ou arrêter le programme.""" 
