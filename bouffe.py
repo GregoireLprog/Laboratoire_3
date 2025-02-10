69e34a4bd2a9053e27247d36420cd943ef533a3a
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-02-10 14:33:11 -0500

    Update bouffe.py

--- bouffe.py
+++ bouffe.py
@@ -1,7 +1,7 @@
 liste = ["pomme", "tarte", "viande"]
 #itère à travers la liste pour tester si c'est une tarte
-for element in liste:
-    if element == "tarte":
+for miam in liste:
+    if miam == "tarte":
         print("c'est une tarte")
     else:
         print("ce n'est pas une tarte")
