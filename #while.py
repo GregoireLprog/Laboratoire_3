65327cfedf7e7a09371b83dbdb8861de8f0c95cf
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-02-10 14:29:35 -0500

    exrc boucle

---
+++ #while.py
@@ -0,0 +1,7 @@
+#while
+# est-ce que la personne (input) a une technique?
+
+reponse = "non"
+while reponse != "oui":
+    reponse = input("Avez-vous une technique?")
+print ("WOW")
---
+++ bouffe.py
@@ -0,0 +1,7 @@
+liste = ["pomme", "tarte", "viande"]
+#itère à travers la liste pour tester si c'est une tarte
+for element in liste:
+    if element == "tarte":
+        print("c'est une tarte")
+    else:
+        print("ce n'est pas une tarte")
